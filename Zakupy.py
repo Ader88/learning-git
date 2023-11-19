86eb34bdb1fb88ad0f39b675b62ccbd4c06cf3ee
Adrian Potoniec <ader-88@wp.pl>
2023-11-19 13:33:12 +0100

    del lines in code

--- Zakupy.py
+++ Zakupy.py
@@ -7,8 +7,3 @@ print("L I S T A  Z A K U P Ó W :")
 suma_produktow = 0
 for sklep, produkty in lista_zakupow.items():
     sklep_wielkimi = sklep.upper()
-    produkty_wielkimi = [produkt.upper() for produkt in produkty]
-    print(f"Idę do {sklep_wielkimi}, kupuję tu następujące rzeczy: {produkty_wielkimi}.")
-    suma_produktow += len(produkty)
-print(f"W sumie kupuję {suma_produktow} produktów.")
-print("......................................")
