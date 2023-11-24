dad77452881767e988a5b5b3d9b8c09c4e581f73
Adrian Potoniec <ader-88@wp.pl>
2023-11-24 19:42:20 +0100

    Add words 2B checked in palindrom checker.

--- palindrom_checker.py
+++ palindrom_checker.py
@@ -4,3 +4,11 @@ def czy_palindrom(tekst):
         if tekst[i] != tekst[len(tekst)-1-i]:
             return False
         return True
+print(czy_palindrom("kajak"))
+print(czy_palindrom("potop"))
+print(czy_palindrom("hello"))
+print(czy_palindrom("kamil"))
+print(czy_palindrom("ślimak"))
+print(czy_palindrom("anna"))
+print(czy_palindrom("kamil ślimak"))
+print(czy_palindrom("atak kata"))
