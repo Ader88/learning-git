8fb2ffb694406de0890717ac652a295ce952c996
Adrian Potoniec <ader-88@wp.pl>
2023-11-24 19:18:38 +0100

    Add rules 4 returning a True or False result

--- palindrom_checker.py
+++ palindrom_checker.py
@@ -1,2 +1,6 @@
 def czy_palindrom(tekst):
     tekst = tekst.replace(" ", "")
+    for i in range(len(tekst)//2):
+        if tekst[i] != tekst[len(tekst)-1-i]:
+            return False
+        return True
